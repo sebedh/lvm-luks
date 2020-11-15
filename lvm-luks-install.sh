96d2c48ee1c2556bcae17a3e6f8e45dc0518e0e3
Sebastian Edholm <sebbe@sebb.io>
2020-11-15 19:35:06 +0100

    fixed so crypt_uuid variable works with sed

--- lvm-luks-install.sh
+++ lvm-luks-install.sh
@@ -142,8 +142,7 @@ if [ "${DISTRIBUTION}" = "NixOS" ]; then
 	nixos-generate-config --root /mnt
 	CRYPT_UUID=`blkid | grep "TYPE=\"crypto_LUKS\"" | cut -d "\"" -f2`
 	echo "$CRYPT_UUID" > /tmp/uuid_crypt
-	sed -ei '/boot.loader.grub.version*/a \ \ boot.initrd.luks.devices.crypted.device = \"/dev/disk/by-uuid/${CRYPT_UUID}\";';
-
+	sed -ei '/boot.loader.grub.version*/a \ \ boot.initrd.luks.devices.crypted.device = \"/dev/disk/by-uuid/'"$CRYPT_UUID"'\";';
 fi
 
 echo "Done, now continue manually, saved uuid of crypt device in /tmp/uuid_crypt"
